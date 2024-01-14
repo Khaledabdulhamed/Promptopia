71883d22ed9990e8a7c9201726abbb865bb79405
Khaled Abdulhamid <khaledraedabdulhamed101@hotmail.com>
2024-01-13 22:21:22 -0500

    second commit

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -5,8 +5,9 @@ import { useState, useEffect } from "react"
 import {signIn, signOut, useSession, getProviders} from 'next-auth/react'
 
 const Nav = () => {
-  const isUserLoggedin = false
+const {data: session} = useSession()
   const [providers, setProviders] = useState(null)
+  const [toggleDropdown, setToggleDropdown ] = useState(false)
 
   useEffect(()=>{
     const setProviders = async () => {
@@ -25,7 +26,7 @@ const Nav = () => {
     </Link>
 
     <div className="sm:flex hidden">
-    {isUserLoggedin ? (
+    {session?.user ? (
       <div className="flex gap-3 md:gap-5">
       <Link href="/create-prompt" className="black_btn">
         Create Post
@@ -39,14 +40,49 @@ const Nav = () => {
       </div> ): (
         <>
         {providers && Object.values(providers).map((provider)=> (
-          <button>
-
+          <button type="button" key={provider.name} onClick={() =>signIn(provider.id)} className="black_btn">
+          Sign In
           </button>
         ))}
         </>
       )
     }
     </div>
+
+    <div className="sm:hidden flex relative">
+      {session?.user ? (<div className="flex "><Image 
+      src="/assets/images/logo.svg" 
+      width={37} height={37}
+       className="rounded-full" alt="profile" onClick={() => setToggleDropdown((prev) => !prev)}/>
+
+       {toggleDropdown && 
+       <div className="dropdown">
+       <Link
+       href="/profile"
+       className="dropdown_link"
+       onClick={() => setToggleDropdown(false)}
+       >My Profile</Link> 
+
+       <Link
+       href="/create-prompt"
+       className="dropdown_link"
+       onClick={() => setToggleDropdown(false)}
+       >Create Prompts</Link>
+      <button type="button" onClick={()=> {setToggleDropdown(false); signOut();}}>
+
+      </button>
+       </div> }
+       </div>) : (<>
+       
+        {providers && Object.values(providers).map((provider)=> (
+          <button type="button" key={provider.name} onClick={() =>signIn(provider.id)} className="black_btn">
+          Sign In
+          </button>
+        ))}
+        </>)}
+    </div>
+
+
     </nav>
   )
 }
