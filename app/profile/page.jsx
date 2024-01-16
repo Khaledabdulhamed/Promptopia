34e8f6d1b680eb858e7da322b716ecd8f555f7dc
Khaled Abdulhamid <khaledraedabdulhamed101@hotmail.com>
2024-01-15 23:38:06 -0500

    Adding the edit delete features

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -7,7 +7,7 @@ import { useState, useEffect } from "react"
 
 
 const MyProfile = () => {
-
+const router = useRouter();
     const {data: session} = useSession()
     const [posts, setPosts] = useState([])
     useEffect(() => {
@@ -21,11 +21,24 @@ const MyProfile = () => {
 
 
 
-    const handleEdit = () => {
-
+    const handleEdit = (post) => {
+      router.push(`/update-prompt?id=${post._id}`)
     }
-    const handleDelete = async () => {
-        
+
+    const handleDelete = async (post) => {
+        const hasConfirmed = confirm("Are you sure you want to delete this Prompt?")
+        if(hasConfirmed) 
+
+        try {
+          await fetch(`/api/prompt/${post._id.toString()}`, {
+            method: 'DELETE'
+          });
+
+          const filteredPosts = posts.filter((p) => p._id !== post._id)
+          setPosts(filteredPosts);
+        } catch (error) {
+          console.log(error)
+        }
     }
 
   return (
